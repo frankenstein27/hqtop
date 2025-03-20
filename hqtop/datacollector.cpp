413e9c525ab8a4dd46dce55bfc3c6dfddf559706
frankenstein27 <3311649589@qq.com>
2025-03-20 17:01:25 +0800

    修正数据采集层

---
+++ hqtop/datacollector.cpp
@@ -0,0 +1,27 @@
+#include "datacollector.h"
+
+DataCollector::DataCollector()
+{
+
+}
+
+// 更新数据（定时更新）
+void DataCollector::dataUpdated(const QList<ProcessInfo>& processes,
+                                    const SystemResource& resource)
+{
+
+}
+
+void DataCollector::startCollection()
+{
+
+}
+void DataCollector::stopCollection()
+{
+
+}
+
+void DataCollector::fetchData()
+{
+
+}
--- hqtop/datacollector.h
+++ hqtop/datacollector.h
@@ -1,6 +1,8 @@
 #ifndef DATACOLLECTOR_H
 #define DATACOLLECTOR_H
 
+#include <QList>
+
 #include "systemdataprovider.h"
 
 // 数据采集层
@@ -8,11 +10,24 @@ class DataCollector
 {
 public:
     DataCollector();
+    // 带参构造函数：用于在高层模块中传递收集者给收集层 由收集者直接与下层交互 降低耦合度
+    explicit DataCollector(SystemDataProvider *provider):provider(provider){};
 
+    void startCollection();
+    void stopCollection();
+
+public slots:
+    void dataUpdated(const QList<ProcessInfo>& processes,
+                            const SystemResource& resource);
+
+protected:
+    void fetchData();
 
 private:
     SystemDataProvider *provider;
-
+    bool running = false;
+    QTime* m_timmer;
+    // QList<ProcessInfo> processes;
 
 };
 
